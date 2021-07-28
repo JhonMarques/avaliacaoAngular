6577b0038fdd4a463f38c4f4c4208b9c22889787
JhonMarques <jhonatasbcc@gmail.com>
2021-07-28 02:45:14 -0300

    cards finalizados

--- src/app/components/views/tela-inicial/tela-inicial.component.ts
+++ src/app/components/views/tela-inicial/tela-inicial.component.ts
@@ -1,3 +1,4 @@
+import { Variable } from '@angular/compiler/src/render3/r3_ast';
 import { Component, OnInit } from '@angular/core';
 
 
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tela-inicial.component.css']
 })
 export class TelaInicialComponent implements OnInit {
-
+ 
+  matriz: any[11][11] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  imagem = true;
+  status = "";
 
   valor1 = 1;
   valor2 = 0;
@@ -16,7 +20,7 @@ export class TelaInicialComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-   
+
   }
 
   
@@ -26,6 +30,20 @@ resultFibonacci(){
     this.valor1 = this.valor2;
     this.valor2 = this.valor3;
 
+}
+
+btnClick(){
+  this.imagem = !this.imagem;
+  return this.imagem;
+}
+
+visivel(){
+  if (this.imagem == true){
+    this.status = "Visível";
+  }else{
+    this.status = "Invisível";
+  }
+  return this.status;
 }
 
 
